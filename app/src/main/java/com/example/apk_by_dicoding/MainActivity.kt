1fc6821032648b05a7d5407e95bb5e68cf0e939a
Oby12 <bobbysaputra514@gmail.com>
2023-09-03 15:38:46 +0700

    saya mecoba materi view binding pada project tersebut

--- app/src/main/java/com/example/apk_by_dicoding/MainActivity.kt
+++ app/src/main/java/com/example/apk_by_dicoding/MainActivity.kt
@@ -6,29 +6,44 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.apk_by_dicoding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var edtWidth: EditText
+   /* private lateinit var edtWidth: EditText
     private lateinit var edtHeight: EditText
     private lateinit var edtLength: EditText
     private lateinit var btnCalculate: Button
-    private lateinit var tvResult: TextView
+    private lateinit var tvResult: TextView*/
 
 
-    private  fun InitComponents(){
+   /* private  fun InitComponents(){
         edtWidth = findViewById(R.id.edt_width)
         edtHeight = findViewById(R.id.edt_height)
         edtLength = findViewById(R.id.edt_length)
         btnCalculate = findViewById(R.id.btn_calculate)
         tvResult = findViewById(R.id.tv_result)
+    }*/
+
+    companion object {
+        private const val STATE_RESULT = "state_result"
     }
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        InitComponents()
+        binding.btnCalculate.setOnClickListener(this)
+       // InitComponents()
 
+        if (savedInstanceState != null) {
+            val result = savedInstanceState.getString(STATE_RESULT)
+            binding.tvResult.text = result
+        }
+/*
         btnCalculate.setOnClickListener {
             val inputLength = edtLength.text.toString().trim()
             val inputWidth = edtWidth.text.toString().trim()
@@ -50,7 +65,15 @@ class MainActivity : AppCompatActivity() {
                 val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
                 tvResult.text = volume.toString()
             }
-        }
+        }*/
     }
+
+    private fun setContentView(root: Any) {
+        TODO("Not yet implemented")
+    }
+}
+
+private fun Button.setOnClickListener(mainActivity: MainActivity) {
+
 }
 
